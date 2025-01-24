6de50cfa3a96be14a5364ab90194a49b56a08aa5
Muskan-Shah-6 <shahmadarmuskan48@gmail.com>
2025-01-24 16:41:09 +0530

    office
    office

---
+++ latest_tailwind_react/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+          <h1 className="text-4xl font-bold text-blue-500">Test, Tailwind!</h1>
+      </div>
+  );
+}
+
+export default App;
